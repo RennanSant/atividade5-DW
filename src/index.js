53ca43ab82dea0745fb43c5ef432f91f4ae06027
Rennan Santiago <61951333+RennanSant@users.noreply.github.com>
2021-11-29 22:47:19 -0300

    primeiro up

---
+++ src/index.js
@@ -0,0 +1,25 @@
+import { dir } from './lib/dir.js';
+import express from 'express';
+
+const app = express();
+
+app.listen(3000, () => {
+    console.log('App running at http://localhost:3000');
+});
+
+app.get('/', (req, res) => {
+    const result = dir();
+    res.json(result);
+});
+
+app.get('/dir', (req, res) => {
+    const path = String(req.query.path);
+    //console.log(path);
+
+    const result = {
+      path,
+      dir: dir(path),
+    };
+
+    res.json(result);
+  });  
---
+++ src/lib/dir.js
@@ -0,0 +1,14 @@
+import { promisify } from "util";
+import { exec } from "child_process";
+
+const execAsync = promisify(exec);
+
+export async function dir(path="") {
+    const command = `dir ${path}`;
+
+    const result = await execAsync(command);
+
+    console.log('dir', result);
+
+    return [];
+};
